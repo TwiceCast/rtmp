1e9d6245b82fb11a104de64043a4ca32c57c7462
Stargateur <antoine.plaskowski@epitech.eu>
2016-10-27 00:46:27 +0200

    fix compile on windows

--- rtmp-sys/build.rs
+++ rtmp-sys/build.rs
@@ -3,6 +3,6 @@ extern crate cmake;
 fn main() {
     let dst = cmake::Config::new("librtmp").build_target("rtmp").build();
 
-    println!("cargo:rustc-link-search=native={}\\build\\Release", dst.display());
+    println!("cargo:rustc-link-search=native={}/build/Release", dst.display());
     println!("cargo:rustc-link-lib=static=rtmp");
 }
